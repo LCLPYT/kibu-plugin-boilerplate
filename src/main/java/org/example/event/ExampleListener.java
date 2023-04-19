5f07af3cca6a9b4ba6561393059482b0824b6895
LCLP <lukas@lclpnet.work>
2023-04-19 09:07:23 +0200

    fix example hook compile error

--- src/main/java/org/example/event/ExampleListener.java
+++ src/main/java/org/example/event/ExampleListener.java
@@ -3,7 +3,7 @@ package org.example.event;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import org.slf4j.Logger;
-import work.lclpnet.kibu.hook.player.PlayerHooks;
+import work.lclpnet.kibu.hook.player.PlayerConnectionHooks;
 import work.lclpnet.kibu.plugin.hook.HookListenerModule;
 import work.lclpnet.kibu.plugin.hook.HookRegistrar;
 
@@ -18,10 +18,10 @@ public class ExampleListener implements HookListenerModule {
     @Override
     public void registerListeners(HookRegistrar registrar) {
         // register hooks here
-        registrar.registerHook(PlayerHooks.JOIN, this::onJoin);
+        registrar.registerHook(PlayerConnectionHooks.JOIN_MESSAGE, this::onJoinMessage);
     }
 
-    private Text onJoin(ServerPlayerEntity player, Text joinMessage) {
+    private Text onJoinMessage(ServerPlayerEntity player, Text joinMessage) {
         logger.info("Player joined: {}", player.getName().getString());
         return joinMessage;
     }
